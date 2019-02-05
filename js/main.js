d6610813e97c923d3497a846bd413f403936ffcf
varunlvs <42297561+varunlvs@users.noreply.github.com>
2019-02-05 10:10:46 +0530

    Captcha

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
             Preloader
 ======================================================*/
 
-$(window).on('load',function(){
-  $('#status').fadeOut();
-  $('#preloader').delay(350).fadeOut('slow');
-});
+// $(window).on('load',function(){
+//   $('#status').fadeOut();
+//   $('#preloader').delay(350).fadeOut('slow');
+// });
 
 // END
  
